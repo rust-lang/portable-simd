9f92c51d89e761cbb19a0898b772817c90a049f7
calebzulawski <calebzulawski@users.noreply.github.com>
2021-02-10 00:38:08 +0000

    deploy: 8aa7ba7d4c1ac9b9e58969dc1558b29e9b02a48a

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["core_simd"] = {"name":"","dirs":[{"name":"masks","dirs":[{"name":"full_masks","files":["mod.rs"]}],"files":["mod.rs"]}],"files":["fmt.rs","intrinsics.rs","lib.rs","macros.rs","ops.rs","permute.rs","round.rs","vectors_f32.rs","vectors_f64.rs","vectors_i128.rs","vectors_i16.rs","vectors_i32.rs","vectors_i64.rs","vectors_i8.rs","vectors_isize.rs","vectors_u128.rs","vectors_u16.rs","vectors_u32.rs","vectors_u64.rs","vectors_u8.rs","vectors_usize.rs"]};
+sourcesIndex["core_simd"] = {"name":"","dirs":[{"name":"masks","files":["bitmask.rs","full_masks.rs","mod.rs"]}],"files":["fmt.rs","intrinsics.rs","lib.rs","macros.rs","ops.rs","permute.rs","round.rs","vectors_f32.rs","vectors_f64.rs","vectors_i128.rs","vectors_i16.rs","vectors_i32.rs","vectors_i64.rs","vectors_i8.rs","vectors_isize.rs","vectors_u128.rs","vectors_u16.rs","vectors_u32.rs","vectors_u64.rs","vectors_u8.rs","vectors_usize.rs"]};
 createSourceSidebar();
