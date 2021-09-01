1362c88f1040bf7be7d81850de0a0054ad9d6f94
Caleb Zulawski <caleb.zulawski@gmail.com>
2021-09-01 10:15:43 -0400

    Fix fmt

--- crates/core_simd/src/lib.rs
+++ crates/core_simd/src/lib.rs
@@ -9,10 +9,7 @@
     staged_api,
     stdsimd
 )]
-#![cfg_attr(
-    feature = "generic_const_exprs",
-    feature(generic_const_exprs)
-)]
+#![cfg_attr(feature = "generic_const_exprs", feature(generic_const_exprs))]
 #![warn(missing_docs)]
 #![unstable(feature = "portable_simd", issue = "86656")]
 //! Portable SIMD module.
