a5f57a6e1e32c8abc63fd969ac6284f8c3714e6e
workingjubilee <workingjubilee@users.noreply.github.com>
2021-12-03 01:42:49 +0000

    deploy: a8385522ade6f67853edac730b5bf164ddb298fd

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["core_simd"] = {"name":"","dirs":[{"name":"masks","files":["full_masks.rs"]},{"name":"vector","files":["float.rs","int.rs","ptr.rs","uint.rs"]},{"name":"vendor","files":["x86.rs"]}],"files":["comparisons.rs","fmt.rs","intrinsics.rs","iter.rs","lane_count.rs","lib.rs","masks.rs","math.rs","mod.rs","ops.rs","reduction.rs","round.rs","select.rs","swizzle.rs","to_bytes.rs","vector.rs","vendor.rs"]};
+sourcesIndex["core_simd"] = {"name":"","dirs":[{"name":"masks","files":["full_masks.rs"]},{"name":"ops","files":["assign.rs","deref.rs","unary.rs"]},{"name":"vector","files":["float.rs","int.rs","ptr.rs","uint.rs"]},{"name":"vendor","files":["x86.rs"]}],"files":["comparisons.rs","fmt.rs","intrinsics.rs","iter.rs","lane_count.rs","lib.rs","masks.rs","math.rs","mod.rs","ops.rs","reduction.rs","round.rs","select.rs","swizzle.rs","to_bytes.rs","vector.rs","vendor.rs"]};
 sourcesIndex["test_helpers"] = {"name":"","files":["array.rs","biteq.rs","lib.rs"]};
 createSourceSidebar();
