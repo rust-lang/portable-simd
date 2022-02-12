195a676e088804cd7d18467e5e29f7737a379a0d
Caleb Zulawski <caleb.zulawski@gmail.com>
2022-02-12 01:16:18 +0000

    Remove ToBitMaskArray

--- crates/core_simd/src/masks.rs
+++ crates/core_simd/src/masks.rs
@@ -13,7 +13,7 @@
 mod mask_impl;
 
 mod to_bitmask;
-pub use to_bitmask::{ToBitMask, ToBitMaskArray};
+pub use to_bitmask::ToBitMask;
 
 use crate::simd::{intrinsics, LaneCount, Simd, SimdElement, SupportedLaneCount};
 use core::cmp::Ordering;
