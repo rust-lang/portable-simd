2ea6d23f0f11e5942c664d04376b3e9e9fe02e16
calebzulawski <calebzulawski@users.noreply.github.com>
2023-11-19 03:05:34 +0000

    deploy: 7e5c03a33db5548fbd78a22656a60574dbf0788f

--- src-files.js
+++ src-files.js
@@ -1,5 +1,5 @@
 var srcIndex = JSON.parse('{\
-"core_simd":["",[["masks",[],["full_masks.rs","to_bitmask.rs"]],["ops",[],["assign.rs","deref.rs","shift_scalar.rs","unary.rs"]],["simd",[["cmp",[],["eq.rs","ord.rs"]],["num",[],["float.rs","int.rs","uint.rs"]],["ptr",[],["const_ptr.rs","mut_ptr.rs"]]],["cmp.rs","num.rs","prelude.rs","ptr.rs"]],["vendor",[],["x86.rs"]]],["alias.rs","cast.rs","fmt.rs","intrinsics.rs","iter.rs","lane_count.rs","lib.rs","masks.rs","mod.rs","ops.rs","select.rs","swizzle.rs","swizzle_dyn.rs","to_bytes.rs","vector.rs","vendor.rs"]],\
+"core_simd":["",[["masks",[],["full_masks.rs"]],["ops",[],["assign.rs","deref.rs","shift_scalar.rs","unary.rs"]],["simd",[["cmp",[],["eq.rs","ord.rs"]],["num",[],["float.rs","int.rs","uint.rs"]],["ptr",[],["const_ptr.rs","mut_ptr.rs"]]],["cmp.rs","num.rs","prelude.rs","ptr.rs"]],["vendor",[],["x86.rs"]]],["alias.rs","cast.rs","fmt.rs","intrinsics.rs","iter.rs","lane_count.rs","lib.rs","masks.rs","mod.rs","ops.rs","select.rs","swizzle.rs","swizzle_dyn.rs","to_bytes.rs","vector.rs","vendor.rs"]],\
 "std_float":["",[],["lib.rs"]],\
 "test_helpers":["",[],["array.rs","biteq.rs","lib.rs","subnormals.rs"]]\
 }');
