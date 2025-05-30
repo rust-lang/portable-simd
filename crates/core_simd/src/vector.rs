cfc7516bb7df78c1d789e73fad357ef778946dd0
Jacob Lifshay <programmerjake@gmail.com>
2025-05-29 20:11:39 -0700

    simd/array-of-simd conversion: describe how size pairs are chosen

--- crates/core_simd/src/vector.rs
+++ crates/core_simd/src/vector.rs
@@ -1120,6 +1120,7 @@ macro_rules! impl_simd_array_conversions {
     };
 }
 
+// this covers all `A`, `B` pairs where `A >= 2 && B >= 2 && A * B <= 64`
 impl_simd_array_conversions! {
     2, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
         17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32];
