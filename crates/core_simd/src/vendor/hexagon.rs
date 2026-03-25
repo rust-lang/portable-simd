39c18af50320a91b7e773385b757fda8b0152421
Brian Cain <brian.cain@oss.qualcomm.com>
2026-03-25 09:00:50 -0700

    fixup! Add support for Hexagon HVX
    fixup! Add support for Hexagon HVX
    
    Address reviewer feedback:
    - Remove hexagon_hvx test file (existing tests suffice with -C flags)
    - Move HvxVector imports into their respective cfg modules
    - Change u8x128/i8x128 comment to FIXME for discoverability

--- crates/core_simd/src/vendor/hexagon.rs
+++ crates/core_simd/src/vendor/hexagon.rs
@@ -2,17 +2,12 @@
 
 use crate::simd::*;
 
-#[cfg(target_feature = "hvx-length128b")]
-use core::arch::hexagon::v128::HvxVector;
-
-#[cfg(not(target_feature = "hvx-length128b"))]
-use core::arch::hexagon::v64::HvxVector;
-
 // HVX 128-byte mode (1024-bit vectors)
 // Enable with: -C target-feature=+hvx-length128b
 #[cfg(target_feature = "hvx-length128b")]
 mod hvx_128b {
     use super::*;
+    use core::arch::hexagon::v128::HvxVector;
 
     // Full vectors (1024-bit) map to HvxVector
     from_transmute! { unsafe u16x64 => HvxVector }
@@ -22,7 +17,7 @@ mod hvx_128b {
     from_transmute! { unsafe u64x16 => HvxVector }
     from_transmute! { unsafe i64x16 => HvxVector }
 
-    // Note: u8x128/i8x128 don't exist in portable-simd
+    // FIXME: u8x128/i8x128 don't exist in portable-simd (max lane count is 64)
     // u8x64/i8x64 are only 512-bit (half of HvxVector in 128B mode)
 }
 
@@ -31,6 +26,7 @@ mod hvx_128b {
 #[cfg(not(target_feature = "hvx-length128b"))]
 mod hvx_64b {
     use super::*;
+    use core::arch::hexagon::v64::HvxVector;
 
     // Full vectors (512-bit) map to HvxVector
     from_transmute! { unsafe u8x64 => HvxVector }
