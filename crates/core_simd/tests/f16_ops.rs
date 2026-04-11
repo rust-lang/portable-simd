3be2371c1cc52904705f4852b91951bd9d6bcb46
Caleb Zulawski <caleb.zulawski@gmail.com>
2026-04-11 11:10:57 -0400

    Add reference to LLVM issue causing f16 wasm ICE

--- crates/core_simd/tests/f16_ops.rs
+++ crates/core_simd/tests/f16_ops.rs
@@ -4,5 +4,7 @@
 #[macro_use]
 mod ops_macros;
 
+// FIXME: some f16 operations cause rustc to hang on wasm simd
+// https://github.com/llvm/llvm-project/issues/189251
 #[cfg(not(all(target_arch = "wasm32", target_feature = "simd128")))]
 impl_float_tests! { f16, i16 }
